c4859f78b0fa8eccf1250b15f764b583ed0dc246
alvquiJuan <juan.alvarez.quinonez@correounivalle.edu.co>
2016-11-18 00:48:13 -0500

    Update ejemplo2.c

--- ejemplo2.c
+++ ejemplo2.c
@@ -12,14 +12,32 @@ int main(int argc, char **argv) {
         MPI_Comm_size(MPI_COMM_WORLD, &p);
         if (my_rank == 0) {
                 strcpy(msg, "Hello ");
+                //Modifique el programa de modo que el maestro envie una cadena y después espere por la cadena modificada 
+
                 MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
+                //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("enviado desde %d el mensaje: %s\n",my_rank,msg);
                 MPI_Recv(msg,20,MPI_CHAR,1,tag,MPI_COMM_WORLD,&status);
+                 //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("recibido en %d el mensaje %s",my_rank ,msg);
         } else if (my_rank == 1) {
                 MPI_Recv(msg, 20, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+                 //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("recibido en %d el mensaje %s",my_rank ,msg);
+                char tmp[20];
+                strcpy(tmp,msg);
+                for (int i=0; i<strlen(tmp);i++){
+                        //cambia mayúsculas por minúsculas
+                        if (65<tmp[i]&& tmp[i]<90){
+                        msg[i]= tmp[i] + 32;
+                        }
+                        //cambia minúsculas por mayúsculas
+                        if (97<tmp[i]&& tmp[i]<122){
+                        msg[i]= tmp[i] - 32;
+                        }
+                }
                 MPI_Send(msg, strlen(msg)+1,MPI_CHAR,0,tag, MPI_COMM_WORLD);
+                 //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("enviado desde %d el mensaje: %s\n",my_rank,msg);
 }
         MPI_Finalize();
