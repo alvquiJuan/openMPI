044cc1c0f8828f19672149460d3b557b1522a20c
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2016-11-18 21:19:31 -0500

    Update ejemplo2.c

--- ejemplo2.c
+++ ejemplo2.c
@@ -5,6 +5,7 @@
 int main(int argc, char **argv) {
         int my_rank, p, tag = 0;
         char msg[20];
+        char msg_mod[20];
         MPI_Status status;
 
         MPI_Init(&argc, &argv);
@@ -12,15 +13,35 @@ int main(int argc, char **argv) {
         MPI_Comm_size(MPI_COMM_WORLD, &p);
         if (my_rank == 0) {
                 strcpy(msg, "Hello ");
+                //envío y recepción en un solo comando, si se desea usar comados indepencientes comentar la lína siguente
+                MPI_Sendrecv (&msg,1,strlen(msg) + 1, MPI_CHAR, 1,tag,&msg_mod,strlen(msg_mod)+1,MPI_CHAR,1,tag, MPI_COMM_WORLD,&Stat); 
                 //Modifique el programa de modo que el maestro envie una cadena y después espere por la cadena modificada 
-
+/* // este bloque envía tareas independientes de envío y recepción 
+ //estas instrucciones deben ejecutarse en el orden establecido puesto que ejecutarlas al revés puede causar un deadlock
                 MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
+                MPI_Recv(msg,20,MPI_CHAR,1,tag,MPI_COMM_WORLD,&status);
+                
+*/
                 //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("enviado desde %d el mensaje: %s\n",my_rank,msg);
-                MPI_Recv(msg,20,MPI_CHAR,1,tag,MPI_COMM_WORLD,&status);
                  //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("recibido en %d el mensaje %s",my_rank ,msg);
+                
         } else if (my_rank == 1) {
+                //envío y recepción en un solo comando, si se desea ussr comandos independiente comentar desde aquí hasta la línea 42
+                MPI_Sendrecv (&msg_mod,strlen(msg_mod)+1, MPI_CHAR, 0,tag,&msg,strlen(msg)+1,MPI_CHAR,0,tag, MPI_COMM_WORLD,&Stat); 
+                int i;
+                for (i=0; i<strlen(msg);i++){
+                        //cambia mayúsculas por minúsculas
+                        if (65<msg[i]&& msg[i]<90){
+                        msg_mod[i]= msg[i] + 32;
+                        }
+                        //cambia minúsculas por mayúsculas
+                        if (97<tmp[i]&& msg[i]<122){
+                        msg_mod[i]= msg[i] - 32;
+                        }
+                }
+  /* // este bloque envía tareas independientes de envío y recepción 
                 MPI_Recv(msg, 20, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
                  //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("recibido en %d el mensaje %s",my_rank ,msg);
@@ -38,6 +59,7 @@ int main(int argc, char **argv) {
                         }
                 }
                 MPI_Send(msg, strlen(msg)+1,MPI_CHAR,0,tag, MPI_COMM_WORLD);
+                */
                  //Haga los ajustes correspondientes de modo que sea mas explícito para el usuario final que se han enviado y recibido los datos
                 printf("enviado desde %d el mensaje: %s\n",my_rank,msg);
 }
